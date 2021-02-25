58f5f4741fe96e9cc241fa50168be168afeddb69
WhoIAm-Benjamin <76595934+WhoIAm-Benjamin@users.noreply.github.com>
2021-02-25 19:53:09 +0300

    Add files via upload

---
+++ 01_lesson_visit-box11.py
@@ -0,0 +1,53 @@
+﻿# coding: utf8
+
+# тестовый скрипт - заходим на сайт visit-box.net
+import urllib
+import urllib.request
+from selenium import webdriver
+import time
+import pyautogui
+
+login='maksim.trya@mail.ru'
+password='mWkskeksqwerterW3'
+
+try:
+    browser = webdriver.Chrome()
+    browser.get("https://visit-box.net/visits/")
+    browser.maximize_window()							# раскрываем окно браузера на весь экран
+    browser.implicitly_wait(5)							# говорим WebDriver искать каждый элемент в течение 5 секунд
+    input1 = browser.find_element_by_css_selector('[name="login"]') # нашли поле Login
+    input1.send_keys(login)
+    input2 = browser.find_element_by_css_selector('[name="password"]') # нашли поле Password
+    input2.send_keys(password)
+    time.sleep(70)
+    url1 = browser.find_element_by_css_selector('[class="title"]') # нашли первую доступную для просмотра ссылку
+    url1.click()
+## решить проблемы
+## 1 - обновлять (F5) - стнаницу - чтобы больше 10-ти
+## 2 - переключатьфокус на 2ю страницу
+    time.sleep(20)
+###################################################################
+# тут я буду пытаться сохранить страницу сайта локально
+# ATTENSION!!!! - Работает ТОЛЬКО если Язык ввода по умолчанию стоит Английский. Просто выбрать Английский язык не поможет
+# ATTENSION!!!! - файл html(вместе с аналогичной подпапкой со js-скриптами картинками и просчим мусором)
+# сохраняется в папку загрузки выбранную по умолчанию, у меня например это папка Загрузки (Downloads)
+# ATTENSION!!!! - при повторном запуске данного скрипта выскочит окно сообщения чтотакой файл уже  существует
+#(по идее или после обработки удаляем данный файл или каждый раз переименовываем его
+# ATTENSION!!!! - при ручном запуске данного сохраненного файла из локальной папки если не хотим получить сообщение:
+# Сбой при загрузке капчи. Повторите попытку - запускаем html-файл без его подпапки со js-скриптами
+# open 'Save as...' to save html and assets
+    pyautogui.hotkey('ctrl', 's')
+    time.sleep(1)
+    pyautogui.typewrite('SEQUENCE.html')
+    pyautogui.hotkey('enter')
+###################################################################
+
+    
+finally:
+    # успеваем скопировать код за 30 секунд
+    
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+#    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
---
+++ 0911_BeautifulSoup_test_121_onvizit.py
@@ -0,0 +1,47 @@
+﻿import os, shutil
+import re 
+from bs4 import BeautifulSoup
+import pathlib
+file = pathlib.Path("1.html")
+news = [] 
+f_save = open('_SAVE.txt', 'w')
+
+with open("1.html", "r", encoding="utf-8") as f_input:
+    contents = f_input.read()
+ 
+soup = BeautifulSoup(contents, 'lxml')
+#    print(soup.select("[data-id]"))
+#    print("\n")
+news=str(soup.select("[data-id]"))
+#print(news)
+words=str(soup.select('[style="padding: 0px; vertical-align: middle;"]'))
+
+# ищем названия графических файлов (капчи), их тут по идее находит 8 штук, но ориганальных только 4, потом они повторяются
+# файлы идут именно в том порядке, как они появляются на экране
+pattern = r'([a-f\d]{10}\.[jpgepn]{3,4})' 
+match_4 = re.findall(pattern, news) 
+
+path = 'c:/1_files/'
+moveto = 'c:/Save/'
+
+for i in range(0, 4):
+    print(match_4[i]) 
+    f_save.write(match_4[i]+'\n')
+    src = path+match_4[i]
+    dst = moveto+match_4[i]
+    shutil.move(path+match_4[i],moveto+match_4[i])
+
+# ищем слова: автомобиль, кошка, собака и т.д. [всего 12 вариантов]
+pattern = r'<b>([а-я]*)</b>' 
+match_1 = re.findall(pattern, words)
+#print(words)
+print(match_1[0])
+f_save.write(match_1[0]+'\n')
+f_input.close()
+f_save.close()
+
+###########################################################################
+shutil.rmtree(path, ignore_errors=True) #- удаляет НЕтолько пустую папку, а ЛЮБУЮ
+#os.remove("/tmp/<file_name>.txt")
+os.remove("1.html")
+###########################################################################
